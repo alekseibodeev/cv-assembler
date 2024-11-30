3b86343b092e5ff0a22cd9a52de990941db1b94f
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-11-30 15:59:45 +0500

    :necktie: Combine everything into app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,25 @@
+import Container from '@/components/container';
+import CVForm from '@/components/cv-form';
 import CVProvider from '@/components/cv-provider';
+import Header from '@/components/header';
+import Preview from '@/components/preview';
 
+import styles from './app.module.css';
 import './index.css';
 import './reset.css';
 
 const App = () => {
   return (
     <CVProvider>
-      <h1>Hello, world!</h1>
+      <Header />
+      <main>
+        <Container>
+          <div className={styles.wrapper}>
+            <CVForm />
+            <Preview />
+          </div>
+        </Container>
+      </main>
     </CVProvider>
   );
 };
